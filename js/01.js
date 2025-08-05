3abceffd39e4e002dd59824e5e0b913ff34523ae
ponkothu <keno.hardwoker@gmail.com>
2025-08-05 10:12:18 +0900

    Add files via upload

---
+++ js/01.js
@@ -0,0 +1,11 @@
+let ramen = document.createElement("li")
+ramen.innerText="ラーメン"
+document.getElementById("meshi").appendChild(ramen)
+
+let apple = document.createElement("li")
+apple.innerText="リンゴ"
+document.getElementById("meshi").appendChild(apple)
+
+let nashi = document.createElement("li")
+nashi.innerText="梨"
+document.getElementById("meshi").appendChild(nashi)
